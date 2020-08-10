59f7dee1a8ac56fad562a916ad6f235e9fa20244
zhentian-wan <answer881215@gmail.com>
2020-08-10 10:46:42 +0300

    actor

--- src/11/index.js
+++ src/11/index.js
@@ -1,42 +1,95 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elApp = document.querySelector('#app');
-const elOffButton = document.querySelector('#offButton');
-const elOnButton = document.querySelector('#onButton');
-const elModeButton = document.querySelector('#modeButton');
+const elApp = document.querySelector("#app");
+const elOffButton = document.querySelector("#offButton");
+const elOnButton = document.querySelector("#onButton");
+const elModeButton = document.querySelector("#modeButton");
 
-const displayMachine = createMachine({
-  initial: 'hidden',
-  states: {
-    hidden: {
-      on: {
-        TURN_ON: 'visible',
+const displayMachine = createMachine(
+  {
+    initial: "hidden",
+    states: {
+      hidden: {
+        on: {
+          TURN_ON: "visible.hist",
+        },
+      },
+      visible: {
+        // Add parallel states here for:
+        type: "parallel",
+        states: {
+          hist: {
+            type: "history",
+            history: "deep",
+          },
+          // - mode (light or dark)
+          mode: {
+            initial: "light",
+            states: {
+              light: {
+                on: {
+                  SWITCH: {
+                    target: "dark",
+                  },
+                },
+              },
+              dark: {
+                on: {
+                  SWITCH: {
+                    target: "light",
+                  },
+                },
+              },
+            },
+          },
+          // - brightness (bright or dim)
+          brightness: {
+            initial: "bright",
+            states: {
+              bright: {
+                after: {
+                  TIMEOUT: {
+                    target: "dim",
+                  },
+                },
+              },
+              dim: {
+                on: {
+                  SWITCH: "bright",
+                },
+              },
+            },
+          },
+        },
+        on: {
+          TURN_OFF: "hidden",
+        },
+        // See the README for how the child states of each of those
+        // parallel states should transition between each other.
       },
     },
-    visible: {
-      // Add parallel states here for:
-      // - mode (light or dark)
-      // - brightness (bright or dim)
-      // See the README for how the child states of each of those
-      // parallel states should transition between each other.
-    },
   },
-});
+  {
+    delays: {
+      TIMEOUT: 2000,
+    },
+  }
+);
 
 const displayService = interpret(displayMachine)
   .onTransition((state) => {
-    elApp.dataset.state = state.toStrings().join(' ');
+    elApp.dataset.state = state.toStrings().join(" ");
   })
   .start();
 
-elOnButton.addEventListener('click', () => {
-  displayService.send('TURN_ON');
+elOnButton.addEventListener("click", () => {
+  displayService.send("TURN_ON");
 });
 
-elOffButton.addEventListener('click', () => {
-  displayService.send('TURN_OFF');
+elOffButton.addEventListener("click", () => {
+  displayService.send("TURN_OFF");
 });
 
-elModeButton.addEventListener('click', () => {
-  displayService.send('SWITCH');
+elModeButton.addEventListener("click", () => {
+  displayService.send("SWITCH");
 });
--- src/12/index.js
+++ src/12/index.js
@@ -1,38 +1,60 @@
-import { createMachine, assign, interpret } from 'xstate';
+import { createMachine, assign, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const randomFetch = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       if (Math.random() < 0.5) {
-        rej('Fetch failed!');
+        rej("Fetch failed!");
       } else {
-        res('Fetch succeeded!');
+        res("Fetch succeeded!");
       }
     }, 2000);
   });
 };
 
 const machine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   states: {
     idle: {
       on: {
-        FETCH: 'pending',
+        FETCH: "pending",
       },
     },
     pending: {
+      on: {
+        RESOLVE: "resolved",
+        // cancel the promise
+        CANCEL: "idle",
+      },
       invoke: {
         // Invoke your promise here.
         // The `src` should be a function that returns the source.
+        src: (context, event) => {
+          return randomFetch();
+        },
+        onDone: {
+          target: "resolved",
+        },
+        onError: {
+          target: "rejected",
+        },
       },
     },
     resolved: {
       // Add a transition to fetch again
+      on: {
+        FETCH: "pending",
+      },
     },
     rejected: {
-      // Add a transition to fetch again
+      // After 2s, retry again
+      after: {
+        2000: {
+          target: "pending",
+        },
+      },
     },
   },
 });
@@ -40,13 +62,18 @@ const machine = createMachine({
 const service = interpret(machine);
 
 service.onTransition((state) => {
-  elBox.dataset.state = state.toStrings().join(' ');
+  elBox.dataset.state = state.toStrings().join(" ");
 
   console.log(state);
 });
 
 service.start();
 
-elBox.addEventListener('click', (event) => {
-  service.send('FETCH');
+elBox.addEventListener("click", (event) => {
+  service.send("FETCH");
+});
+
+const cancelBtn = document.querySelector("#cancel");
+cancelBtn.addEventListener("click", (event) => {
+  service.send("CANCEL");
 });
